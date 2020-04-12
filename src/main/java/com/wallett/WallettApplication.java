83fb94bca3ebe1d64bbd6f1a8a240257216e936c
Glayton <glayton.paula95@gmail.com>
2020-04-12 20:03:50 -0300

    first commit

---
+++ src/main/java/com/wallett/WallettApplication.java
@@ -0,0 +1,13 @@
+package com.wallett;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallettApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallettApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wallett/WallettApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallett;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallettApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
