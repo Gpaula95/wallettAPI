accd26a3b6c4b9c5b157ca9e030600aa77959224
Glayton <glayton.paula95@gmail.com>
2020-04-12 21:10:20 -0300

    finally user repository

--- src/test/java/com/wallett/HelloWorld.java
+++ src/test/java/com/wallett/HelloWorld.java
@@ -1,15 +1,15 @@
 package com.wallett;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
-class WallettApplicationTests {
-
+public class HelloWorld {
+	
 	@Test
-	void contextLoads() {
+	public void testHelloWorld() {
+		assertEquals(1, 1);
 	}
-
 }
