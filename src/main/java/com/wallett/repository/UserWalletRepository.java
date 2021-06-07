8fe02aec4de6899c74fc650ab867b5f6ee8c1141
Glayton <glayton.paula95@gmail.com>
2021-06-07 19:48:45 -0300

    adicionando autenticação spring security

--- src/main/java/com/wallett/repository/UserWalletRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wallett.repository;
-
-import com.wallett.entity.UserWallet;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserWalletRepository extends JpaRepository<UserWallet, Long>{
-
-}
